046c76f641e0beb47312ea6fa3574368589ee45b
nezlioui <idirnezlioui4@gmail.com>
2025-04-09 16:52:53 +0200

    deploiement

--- backend/app.js
+++ backend/app.js
@@ -29,11 +29,12 @@ app.listen(PORT, () => console.log(` Server running on http://localhost:${PORT}`
 const path=require("path")
 
 //servire les fichiers react en production 
-if (process.env.NODE_ENV ==='production') {
-    app.use(express.static(path.join(__dirname,"frontend/build")))
-
-    app.get("*",(req,res)=>{
-        res.sendFile(path.join(__dirname,'frontend/build',indexedDB.html))
-    })
-}
+// Servir les fichiers React en production
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, 'frontend/build')));
+  
+    app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, 'frontend/build', 'index.html'));
+    });
+  }
 //Ça permet de servir automatiquement ton interface React quand l’app est déployée
